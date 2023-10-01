aef6b2d59014c3aed6e02978acb4bf5e32d0d5ff
sofi <sofiamagaliramos@gmail.com>
2023-10-01 12:09:02 -0300

    added comments

--- src/class/Stock.ts
+++ src/class/Stock.ts
@@ -12,21 +12,26 @@ export class Stock {
     getAll () : Array<Product> {
     return this.listaProductos
     }
-    
+    //retornar lista de productos cuando se borra uno
     delete(product: Product, cant: number) {
-       let cont = 1
-        const nuevo: Product [] = this.listaProductos.filter((prod)=> {
-            if(prod != product){
-              return prod
-            }else{
-              if(cont <= cant){
-                cont ++
-              }else{
-                return prod
-              }
-            }
-          })
-          this.listaProductos = nuevo}
-
+      let cont = 1; // Inicializo contador en 1
+    
+      const nuevo: Product[] = this.listaProductos.filter((prod) => {
+        if (prod != product) {
+          // 💻Si el producto no es igual al que quiero eliminar , lo conservo en la nueva lista.
+          return prod;
+        } else {
+          if (cont <= cant) {
+            // Si encuentra el producto que quiero eliminar y todavía no alcanzó la cantidad máxima que se debe eliminar (cant), incrementa el contador.
+            cont++;
+          } else {
+            // Si ya se alcanzó la cantidad máxima de eliminación, se conserva el producto en la nueva lista.
+            return prod;
+          }
+        }
+      });
+    
+      this.listaProductos = nuevo; // Asignamos la nueva lista de productos a this.listaProductos
     }
+    
  
--- src/interfaces/Precio.ts
+++ src/interfaces/Precio.ts
@@ -1,5 +1,6 @@
 export interface Precio {
     price: number;
-
+//cantidad es un parámetro opcional
+//lo uso en caso de que se seleccione el tipo de venta por cantidad o por bolsa
     calcular(cantidad?: number): number;
 }
